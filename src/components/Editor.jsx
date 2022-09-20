1cc2410537f61defed3fe55f870c039b62512dd5
mscalel <msksaul@gmail.com>
2022-09-20 16:30:37 -0600

    Build Update 1

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,6 +1,6 @@
 import { useState }from "react"
 import ReactMde from "react-mde"
-import Showdown from "showdown"
+import * as Showdown from "showdown"
 
 export default function Editor({ currentNote, updateNote }) {
     const [selectedTab, setSelectedTab] = useState("write")
