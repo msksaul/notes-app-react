bac5ba27a01b3d964f00371e633b1b3c54459a77
mscalel <msksaul@gmail.com>
2022-09-20 16:34:27 -0600

    CSS Import

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,6 +1,7 @@
 import { useState }from "react"
 import ReactMde from "react-mde"
 import * as Showdown from "showdown"
+import "react-mde/lib/styles/css/react-mde-all.css";
 
 export default function Editor({ currentNote, updateNote }) {
     const [selectedTab, setSelectedTab] = useState("write")
